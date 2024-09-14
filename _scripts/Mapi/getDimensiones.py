bbab9f0f2ff42bba99233eed82bc379fec4631b6
Andres <andresrocabado99@gmail.com>
2024-09-14 11:51:05 -0400

    dimension indice de incoherencia

--- _scripts/Mapi/getDimensiones.py
+++ _scripts/Mapi/getDimensiones.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 with open('./dimensiones_mapi.txt', 'r', encoding='utf-8') as file:
   textos = file.readlines()
@@ -56,6 +57,87 @@ for texto in textos:
 
   contador += 1
 
+# Para dimension de incoherencia
+dimensiones_dict["id"] = len(dimensiones_procesadas) + 1
+dimensiones_dict["descripcion"] = "INDICE DE INCOHERENCIA"
+dimensiones_dict["abreviacion"] = ""
+
+def split_number_and_letter(s):
+    match = re.match(r"(\d+)([VF])", s)  # Solo coincide con V o F
+    if match:
+        return match.groups()
+    return None
+
+textos_incoherencias = ["1V-137V",
+          "1F-137F",
+          "2V-132V",
+          "2F-132F",
+          "3V-126V",
+          "3F-126F",
+          "4V-17F",
+          "4F-17V",
+          "12V-140F",
+          "12F-140V",
+          "20V-74V",
+          "20F-74F",
+          "23V-119V",
+          "23F-119F",
+          "25V-110V",
+          "25F-110F",
+          "27V-76F",
+          "27F-76V",
+          "29V-123V",
+          "29F-123F",
+          "32V-73V",
+          "32F-73F",
+          "43V-66F",
+          "43F-66V",
+          "45V-144F",
+          "45F-144V",
+          "54V-97V",
+          "54F-97F",
+          "68V-100V",
+          "68F-100F",
+          "104V-143F",
+          "104F-143V"]
+
+incoherencias = []
+
+for texto in textos_incoherencias:
+
+  first_value = split_number_and_letter(texto.split("-")[0])
+  second_value = split_number_and_letter(texto.split("-")[1])
+  first_number = first_value[0]
+  first_option = 0
+  second_number = second_value[0]
+  second_option = 0
+
+  if(first_value[1] == 'V'):
+    first_option = 1
+  else:
+    first_option = 2
+  if(second_value[1] == 'V'):
+    second_option = 1
+  else:
+    second_option = 2
+
+  array_incoherencia = [
+     {
+        "id_pregunta": int(first_number.strip()),
+        "id_opcion": first_option,
+        "puntuacion": 1
+     },
+     {
+        "id_pregunta": int(second_number.strip()),
+        "id_opcion": second_option,
+        "puntuacion": 1
+     }
+  ]
+
+  incoherencias.append(array_incoherencia)
+dimensiones_dict["items"] = incoherencias
+
+dimensiones_procesadas.append(dimensiones_dict.copy())
 
 dimensiones_json = json.dumps(dimensiones_procesadas, ensure_ascii=False, indent=2)
 
